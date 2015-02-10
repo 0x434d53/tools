275a3cf0a5139722a5ca897c897688b7631a9f79
0x434D53 <christoph.seufert@gmail.com>
2015-02-10 21:21:52 +0100

    progess

---
+++ git/git_overview_of_repos/main.go
@@ -0,0 +1,155 @@
+package main
+
+import (
+	"fmt"
+	"github.com/0x434D53/tools/git/lib"
+	"github.com/russross/blackfriday"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"os"
+	"path"
+	"time"
+)
+
+type GitRepo struct {
+	lib.GitInfos
+	ReadMeRendered []byte
+	LastUpdated    time.Time
+	URL            string
+}
+
+type ProjectAndId struct {
+	Id          int
+	Projectname string
+}
+
+type templateData struct {
+	Project            []ProjectAndId
+	CurrentReadme      []byte
+	CurrentProjectname string
+	CurrentLastUpdate  string
+	CurrentUser        string
+	CurrentURL         string
+}
+
+var gitRepos []GitRepo
+var servePath string
+
+func CollectRenderingInformation(gi []lib.GitInfos) ([]GitRepo, error) {
+	gitRepos := make([]GitRepo, 0)
+
+	for _, g := range gi {
+		gr := GitRepo{}
+
+		gr.Path = g.Path
+		gr.Projectname = g.Projectname
+		gr.Username = g.Username
+
+		gr.ReadMeRendered = RenderReadme(gr.Path)
+		gr.LastUpdated = GetLastUpdate(gr.Path)
+		gr.URL = GetURL(g)
+		gitRepos = append(gitRepos, gr)
+	}
+
+	return gitRepos, nil
+}
+
+func GetLastUpdate(path string) time.Time {
+	return time.Now()
+}
+
+func GetURL(gi lib.GitInfos) string {
+	return ""
+}
+
+func RenderReadme(p string) []byte {
+	file, err := findReadme(p)
+
+	if err != nil {
+		return []byte("This project has no README")
+	}
+
+	ext := path.Ext(file)
+
+	f, err := os.Open(file)
+
+	if err != nil {
+		return []byte("This project has no valid README")
+	}
+
+	contents, err := ioutil.ReadAll(f)
+
+	if ext == ".md" || ext == ".MD" {
+		return blackfriday.MarkdownCommon(contents)
+	}
+
+	return contents
+}
+
+func findReadme(p string) (string, error) {
+	filestoCheck := []string{"README.md", "readme.md", "Readme.md", "readme.txt", "README.markdown"}
+
+	for _, f := range filestoCheck {
+		_, err := os.Stat(path.Join(p, f))
+
+		if err == nil {
+			return path.Join(p, f), nil
+		}
+	}
+
+	return "", fmt.Errorf("No Readme found")
+}
+
+func ServeWebSite(servePath string) {
+	log.Panic(http.ListenAndServe(":8081", http.FileServer(http.Dir(servePath))))
+}
+
+func CreateTempDir() (string, error) {
+	tempDir := os.TempDir()
+
+	gitDir := "gitDir" // TODO: add a timestamp
+
+	newDir := path.Join(tempDir, gitDir)
+
+	err := os.Mkdir(newDir, 0755)
+
+	if err != nil {
+		return "", err
+	}
+
+	return newDir, nil
+}
+
+func main() {
+	if len(os.Args) != 2 {
+		fmt.Println("Please give a root path as an argument")
+		return
+	}
+
+	servePath, err := CreateTempDir()
+
+	if err != nil {
+		fmt.Println("Unable to create temporary directory")
+		panic(err)
+	}
+
+	gitInfos, err := lib.CollectGitRepositories(os.Args[1])
+	if err != nil {
+		panic(err)
+	}
+
+	repos, err := CollectRenderingInformation(gitInfos)
+
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Println(repos)
+
+	if err != nil {
+		panic(err)
+	}
+
+	ServeWebSite(servePath)
+}
